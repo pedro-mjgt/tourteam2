83596284df8c154d13385be9cfe7329989ee52ec
pedro-mjgt <pedro.mjgt@gmail.com>
2024-10-25 22:10:29 -0500

    jenkins terminado

---
+++ src/main/java/com/example/tourteam2/Tourteam2Application.java
@@ -0,0 +1,13 @@
+package com.example.tourteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tourteam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tourteam2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tourteam2/Tourteam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourteam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tourteam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
